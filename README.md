a97c94183f84b43145c63bffafebd0562f486bf4
Sergej Kotliar <sergej.kotliar@gmail.com>
2014-04-16 16:06:39 +0200

    write a README

--- README.md
+++ README.md
@@ -2,3 +2,46 @@ node-bitcoin-trader
 ===================
 
 A rudimentary node package to to poll prices and depths from Bitcoin exchanges, and to do trading, thus making it easy to do arbitrage.
+
+NOTE:
+====
+
+This is completely un-tested and work in progress. Consider this non-working at this point. If you use this, it is your own fault. 
+
+Currently supports:
+===
+
+Polling:
+
+* Bitfinex
+* Bitstamp
+* Justcoin
+* Kraken
+
+Trading:
+
+* Justcoin
+* Kraken
+
+Supports automatically calculating arbitrage oportunities.
+
+
+TODO: 
+ 
+* Implement arbitrage mechanisms
+* Implement more exchanges
+* Unit tests
+
+Install
+=======
+
+`npm install;`
+
+`cp config.example.js config.js`
+
+Fill in your keys etc into config.js
+
+`node index.js`
+
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ config.example.js
@@ -0,0 +1,52 @@
+var _ = require('lodash');
+module.exports = {
+    'kraken' : {
+        enabled: false,
+        fee : 0.2 / 100, // best case: 0.05, worst case: 0.3 (USD), 0.2 (EUR)
+        key : '', 
+        secret: '',
+        currencies : ['BTC', 'EUR', 'USD']
+    },
+    'bitstamp' : {
+        enabled: false,
+        fee : 0.5 / 100, // best case: 0.2, worst case: 0.5. Bitstamp fee will auto update
+        key : '', 
+        secret: '', 
+        customer_id: '',
+        currencies : ['BTC', 'USD']
+    },
+    'justcoin' : {
+        enabled: false,
+        key : '',
+        fee : 0.5 / 100, // best case : 0, worst case: 0.5,
+        currencies : ['BTC', 'EUR', 'USD']
+    },
+    'bitfinex' : {
+        enabled : false,
+        balance : { // still manual at this point.
+            'BTC' : 0.0,
+            'USD' : 0.0,
+        },
+        fee : 0.15 / 100, // best case: 0.1
+        key:'', 
+        secret: '',
+        currencies : ['BTC', 'USD']
+    }
+};
+/*
+// uncomment to calculate on zero fees
+_.merge(module.exports, {
+    'kraken' : {
+        fee : 0
+    },
+    'bitstamp' : {
+        fee : 0
+    },
+    'justcoin' : {
+        fee : 0
+    },
+    'bitfinex' : {
+        fee : 0
+    }
+});
+*/
