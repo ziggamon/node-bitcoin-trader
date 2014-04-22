a6824e28eb190f7f0dade0e5806d0b7c02b924d7
Sergej Kotliar <sergej.kotliar@gmail.com>
2014-04-23 00:03:12 +0200

    Cleaned out old style code (in separate branch now), and added a few comments

--- arbitrager.js
+++ arbitrager.js
@@ -7,16 +7,10 @@ var Promise = require('bluebird');
 var request = Promise.promisify(require('request'));
 var config = require('./config.js');
 
-
-
-
 var baseCurrency = 'USD';
 
 var ratesPromise = request('http://openexchangerates.org/api/latest.json?app_id=' + config.rates.app_id, {json:true}).spread(function(request, data){
 	_.extend(fx, data);
-	// console.log('testväxlar: ', fx.convert(1000, {from : "USD", to: "EUR"}));
-
-	// console.log('got data: ', data);
 });
 
 
@@ -31,68 +25,6 @@ function buying_cost(amount, fee){
 }
 function selling_rev(amount, fee){
     return amount * (1 - fee);   
-}
-
-/*
-    Filter function that basically returns if two spreads have a profitable
-    arbitrage oportunity.   
-*/
-function arbitrage_possibilities(from, to){
-    if(Array.isArray(from)){ // handle pairform as well
-        to = from[1];
-        from = from[0];
-    }
-
-//    console.log(from.exchange, from.currency, to.exchange, to.currency);
-    // console.log(from, to);
-    // buying in from market, selling in to-market
-    // console.log(from.exchange, 'asks: ', from.asks.length, to.exchange, 'bids: ', to.bids.length, 'to fee', to.fee, 'from fee', from.fee);
-    var lowest_ask = fx.convert(from.asks[0][0], {
-    	from: from.currency,
-    	to : baseCurrency
-    });
-    var highest_bid = fx.convert(to.bids[0][0], {
-    	from : to.currency,
-    	to: baseCurrency
-    });
-
-    return(selling_rev(highest_bid, to.fee) > buying_cost(lowest_ask, from.fee) * 1); // add 1% requirement margin
-}
-
-/*
-    Compares a given spread data for an exchange with all other exchanges spread data
-    stored, returns array of which spreads an arbitrage oportunity exists between. 
-*/
-function detectCrossCurrencyArbitrageForSpread(spread){
-    // var arbitragePossibilities = [];
-    var spreadPairs = [];
-
-	var allSpreads = [];
-
-	_.forOwn(trader.RawSpreads, function(currencyIndex) { 
-		_.forOwn(currencyIndex, function(oneSpread){
-			allSpreads.push(oneSpread);
-		});
-	});
-
-
-    allSpreads.forEach(function(otherExchange){
-    	if(_.isEqual(spread, otherExchange)){return;} // don't compare with self.
-
-        spreadPairs.push([spread, otherExchange]);
-        spreadPairs.push([otherExchange, spread]);
-        console.log('adding pair: ', spread.exchange, otherExchange.exchange, 'fees: ', spread.fee, otherExchange.fee);
-    });
-
-    var arbitragePossibilities = _.filter(spreadPairs, arbitrage_possibilities);
-
-    if(arbitragePossibilities.length > 0){
-        arbitragePossibilities.forEach(processArbitrage);
-    } else {
-        console.log('no arbitrage possible on ' + spread.exchange);
-    }
-
-    return arbitragePossibilities;
 }
 
 var combinedSpreads = {
@@ -134,6 +66,8 @@ function equivComparatorDesc(a, b){
 }
 
 var currentCurrency, currentExchange, currentFee, currencyMultiplier, feeMultiplier, deFactoMultiplier;
+
+
 function objectifySpreadItem(item){
 	return {
 		price : item[0],
@@ -144,6 +78,10 @@ function objectifySpreadItem(item){
 		deFactoPrice : (item[0] * deFactoMultiplier).toFixed(1)
 	};
 }
+
+/*
+	Maintains a sorted of best trades available in the system
+*/
 function addToEquivIndex(spread){
 	console.log('updated spread data 	', spread.exchange);
 
@@ -231,7 +169,10 @@ function theoreticalTradeAmount(nextTrade, neededAmount){
 	return _.min([nextTrade.amount, neededAmount]);
 }
 
-
+/*
+	Gives you the best trades to buy or sell neededAmount or BTC. 
+	Either practically (takes into account balances) or theoretically.
+*/
 function bestTrade(buySell, neededAmount, theoretical){
 	var availableTrades = _.cloneDeep( (buySell.toLowerCase() === 'buy') ? combinedSpreads.asks : combinedSpreads.bids );
 	var tradeAmount, nextTrade;
@@ -251,38 +192,6 @@ function bestTrade(buySell, neededAmount, theoretical){
 	}
 	return outputTrades;
 }
-
-
-
-/*
-    Compares a given spread data for an exchange with all other exchanges spread data
-    stored, returns array of which spreads an arbitrage oportunity exists between. 
-*/
-function detectArbitrageFor(spread){
-    // var arbitragePossibilities = [];
-    var spreadPairs = [];
-
-
-    var everyOther = _.values(_.omit(trader.RawSpreads[spread.currency], spread.exchange));
-
-    everyOther.forEach(function(otherExchange){
-        spreadPairs.push([spread, otherExchange]);
-        spreadPairs.push([otherExchange, spread]);
-        console.log('adding pair: ', spread.exchange, otherExchange.exchange, 'fees: ', spread.fee, otherExchange.fee);
-    });
-
-    var arbitragePossibilities = _.filter(spreadPairs, arbitrage_possibilities);
-
-    if(arbitragePossibilities.length > 0){
-        arbitragePossibilities.forEach(processArbitrage);
-    } else {
-        console.log('no arbitrage possible on ' + spread.exchange);
-    }
-
-
-    return arbitragePossibilities;
-}
-
 
 /*
     Either simulates or trades on two exchanges if arbitrage possibilities
@@ -343,8 +252,10 @@ function spitArbitrage(gtfo, theoretical){
     return arbitrades;
 }
 
+/*
+	Compress arbitrades so multiple on same exchange with same price get combined.
+*/
 function combineArbitrades(arbitrades){
-	// compress arbitrades so multiple on same exchange with same price get combined.
 	var processedArbitrades = [], combinations = [];
 
 	while(arbitrades.length > 0){
@@ -357,92 +268,6 @@ function combineArbitrades(arbitrades){
 	}
     return processedArbitrades;
 }
-
-function processArbitrage(from, to){
-    if(Array.isArray(from)){ // handle pairform as well
-        to = from[1];
-        from = from[0];
-    }
-
-    console.log('processing arbitrage', from.exchange, from.currency, to.exchange, to.currency);
-
-    var asks = _.cloneDeep(from.asks);
-    var bids = _.cloneDeep(to.bids);
-
-    var lowest_ask = asks.shift();
-    var highest_bid = bids.shift();
-
-    console.log('THEORETICAL best trade: ', fx(lowest_ask[0]).from(from.currency).to(baseCurrency), fx(highest_bid[0]).from(to.currency).to(baseCurrency));
-
-    var mixed_fees = (1+from.fee)*(1+to.fee);
-
-    var buyingBalance = trader.exchanges[from.exchange].balance[from.currency];
-    var sellingAmount = trader.exchanges[to.exchange].balance['BTC'];
-
-    console.log('buying balance: ', buyingBalance, 'selling amount: ', sellingAmount);
-
-    if(buyingBalance < 1){
-        console.log('Dont have fiat on ', from.exchange.toUpperCase());
-        return false;
-    }
-    if(sellingAmount < 0.001){
-        console.log('Dont have crypto on ', to.exchange.toUpperCase());
-        return false;
-    }
-
-    var buyingAffordance = buyingBalance / buying_cost(lowest_ask[0], from.fee);
-
-    var amount, profit, margin, buyCost, sellRev;
-    var totalAmount = 0, totalProfit = 0, totalCost = 0, totalRevenue = 0, totalMargin = 0;
-
-    while (lowest_ask && highest_bid && lowest_ask[0]*mixed_fees < highest_bid[0]){ // while it's still profitable
-        buyCost = buying_cost(lowest_ask[0], from.fee);
-        sellRev = selling_rev(highest_bid[0], to.fee);
-
-        amount = Math.min(sellingAmount, buyingAffordance, lowest_ask[1], highest_bid[1]);
-
-        lowest_ask[1]   -= amount;
-        highest_bid[1]  -= amount;
-        sellingAmount -= amount;
-        buyingAffordance  -= amount;
-
-        profit = (amount * (highest_bid[0] - lowest_ask[0])).toFixed(3);
-        margin = (((highest_bid[0] / lowest_ask[0]) - 1) * 100).toFixed(2);
-
-        if(amount === 0){
-            // console.log(from.asks, to.bids);
-            throw 'ZeroAmount, something went wrong!';
-        }
-        console.log('buy ' + amount + ' @ ' + from.exchange + ' à ' +lowest_ask[0] + ', sell @' + to.exchange + ' à ' + highest_bid[0] + ', profit: ' + profit + ', margin: ' + margin + '%');
-
-        totalCost += (amount * lowest_ask[0]);
-        totalRevenue += (amount * highest_bid[0]);
-
-
-        if(lowest_ask[1] == 0){
-            console.log('full buy');
-            lowest_ask = asks.shift();
-        } 
-        if(highest_bid[1] == 0){
-            console.log('full sell');
-            highest_bid = bids.shift();
-        } 
-        if(sellingAmount == 0){
-            console.log('no more coins to sell');
-            break;
-        }
-        if(buyingAffordance == 0){
-            console.log('no more funds to buy');
-            break;
-        }
-    }
-    totalProfit = totalRevenue - totalCost;
-    totalMargin = (((totalRevenue / totalCost) - 1) * 100).toFixed(2);
-
-    console.log('trade cost : ' + totalCost + ', revenue: ' + totalRevenue + ', profit : ' + totalProfit + ', margin : ' + totalMargin+ '%');
-}
-
-
 
 
 trader.init().then(function () {
@@ -457,9 +282,6 @@ trader.init().then(function () {
 	ratesPromise.then(function(){
 	    trader.watch('EUR');
 	    trader.watch('USD');
-
-	 //    console.log('inited trader!');
-	 //    // console.log();
 
 		// Promise.all(_.map(_.keys(trader.RawSpreads), trader.getAllSpreads, trader)).then(function(){
 		// 	console.log('got all spreads')
